3bf237c0b31937552b005064db9d939af2d4c55d
ivanszhuravlev <ivanszhuravlev@gmail.com>
2023-06-22 00:23:00 +0200

    Add dependencies for FastImage, FlashList and react-query

--- src/RootApp.tsx
+++ src/RootApp.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {ThemeProvider} from 'styled-components/native';
 import {RootStack} from 'navigation/RootStack';
 import {useCurrentTheme} from 'theme';
+
+const queryClient = new QueryClient();
 
 export const RootApp = () => {
   const theme = useCurrentTheme();
 
   return (
     <NavigationContainer>
-      <ThemeProvider theme={theme}>
-        <RootStack />
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <RootStack />
+        </ThemeProvider>
+      </QueryClientProvider>
     </NavigationContainer>
   );
 };
